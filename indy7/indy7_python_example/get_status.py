f427dabbb9a062aca57b16c84d3adf0ec1e82604
soc_notebook <jks8235@g.skku.edu>
2022-07-29 15:22:51 +0900

    waypoint

--- indy7/indy7_python_example/get_status.py
+++ indy7/indy7_python_example/get_status.py
@@ -1,12 +1,14 @@
+# from tracemalloc import start
 from indy_utils import indydcp_client as client
 from indy_utils.indy_program_maker import JsonProgramComponent
 
 import json
 import threading
-from time import sleep
+import time
+
 
 # Set robot (server) IP 
-robot_ip = "10.201.159.232"  # Robot (Indy) IP
+robot_ip = "115.145.159.87"  # Robot (Indy) IP
 
 # Set robot name
 name = "NRMK-Indy7"  # Robot name (Indy7)
@@ -18,11 +20,22 @@ indy = client.IndyDCPClient(robot_ip, name)
 indy.connect()
 
 status = indy.get_robot_status()
+start_time = time.time()
 joint_pos = indy.get_joint_pos()
+end_time = time.time()
+print(1/(end_time - start_time))
 task_pos = indy.get_task_pos()
 
 print(status)
 print(joint_pos)
 print(task_pos)
 
+i=0
+while i<100:
+    i+=1
+    start_time = time.time()
+    joint_pos = indy.get_joint_pos()
+    end_time = time.time()
+    print(1/(end_time - start_time))
+    
 indy.disconnect()
--- indy7/indy7_python_example/pick_place.py
+++ indy7/indy7_python_example/pick_place.py
@@ -18,7 +18,7 @@ import copy
 
 ### indy7 set up ###
 # Set robot (server) IP 
-robot_ip = "10.201.159.232"  # Robot (Indy) IP
+robot_ip = "115.145.159.87"  # Robot (Indy) IP
 
 # Set robot name
 name = "NRMK-Indy7"  # Robot name (Indy7)
@@ -35,18 +35,18 @@ class SingleIndy:
         self.indy7_task_pos = []
 
         # motion velocity, blend setting
-        self.vel = 5
+        self.vel = 3
         self.j_blend = 20
         self.t_blend = 0.2
 
     # running code
     def run_indy7(self):
 
-        self.do_job()
+        self.job_plan()
 
         indy.connect()
 
-        path = self.make_path('mix')
+        path = self.make_path('task')
         indy.set_and_start_json_program(path)
 
 
@@ -73,38 +73,47 @@ class SingleIndy:
         del self.indy7_task_pos[:]
 
     ### setting moving point
-    def do_job(self):
-        # pick ready pos
-        pick_ready_joint_pos = [35.45288085937734, -47.414540692797544, -55.27484988378463, -0.0325782700340368, -77.29947836092711, 35.40246340308092]
-        pick_ready_task_pos = [0.6563396438579246, 0.238244127385817, 0.2997290484637074, 0.019571572691017697, 179.9724920327221, 0.05755109533355176]
+    def job_plan(self):
+        # # pick ready pos
+        # pick_ready_joint_pos = [35.45288085937734, -47.414540692797544, -55.27484988378463, -0.0325782700340368, -77.29947836092711, 35.40246340308092]
+        # pick_ready_task_pos = [0.6563396438579246, 0.238244127385817, 0.2997290484637074, 0.019571572691017697, 179.9724920327221, 0.05755109533355176]
         
-        # pick pos
-        pick_joint_pos = [35.453107849627855, -55.09543678977636, -64.7333520306086, -0.03464500502784882, -60.12980546101881, 35.410893506345154]
-        pick_task_pos = [0.6563170228215546, 0.2384558980769108, 0.155472048578118, -0.001384742620016496, 179.94928598028307, 0.06657559817585178]
+        # # pick pos
+        # pick_joint_pos = [35.453107849627855, -55.09543678977636, -64.7333520306086, -0.03464500502784882, -60.12980546101881, 35.410893506345154]
+        # pick_task_pos = [0.6563170228215546, 0.2384558980769108, 0.155472048578118, -0.001384742620016496, 179.94928598028307, 0.06657559817585178]
 
-        # release ready pos
-        release_ready_joint_pos = [-13.326098229275155, -55.226909542875546, -39.38176430946798, 0.001305306311881274, -85.27680349822653, -13.363236531173529]
-        release_ready_task_pos = [0.656616407444014, -0.34718812991931075, 0.30052026740751203, 0.02514655643285834, 179.8885157762869, 0.03715637064031851]
+        # # release ready pos
+        # release_ready_joint_pos = [-13.326098229275155, -55.226909542875546, -39.38176430946798, 0.001305306311881274, -85.27680349822653, -13.363236531173529]
+        # release_ready_task_pos = [0.656616407444014, -0.34718812991931075, 0.30052026740751203, 0.02514655643285834, 179.8885157762869, 0.03715637064031851]
 
-        # release pos
-        release_joint_pos = [-13.324600093621745, -63.47151322798713, -52.05036257909693, 0.0007614286819307433, -64.3780880163254, -13.362583878017588]
-        release_task_pos = [0.6564373136811749, -0.3472904856316228, 0.12218557901739534, 0.02195777089571179, 179.90590167873245, 0.030532790099343992]
+        # # release pos
+        # release_joint_pos = [-13.324600093621745, -63.47151322798713, -52.05036257909693, 0.0007614286819307433, -64.3780880163254, -13.362583878017588]
+        # release_task_pos = [0.6564373136811749, -0.3472904856316228, 0.12218557901739534, 0.02195777089571179, 179.90590167873245, 0.030532790099343992]
 
-        self.indy7_joint_pos.append(pick_ready_joint_pos)
-        self.indy7_joint_pos.append(pick_joint_pos)
-        self.indy7_joint_pos.append(pick_ready_joint_pos)
+        # self.indy7_joint_pos.append(pick_ready_joint_pos)
+        # self.indy7_joint_pos.append(pick_joint_pos)
+        # self.indy7_joint_pos.append(pick_ready_joint_pos)
 
-        self.indy7_joint_pos.append(release_ready_joint_pos)
-        self.indy7_joint_pos.append(release_joint_pos)
-        self.indy7_joint_pos.append(release_ready_joint_pos)
+        # self.indy7_joint_pos.append(release_ready_joint_pos)
+        # self.indy7_joint_pos.append(release_joint_pos)
+        # self.indy7_joint_pos.append(release_ready_joint_pos)
 
-        self.indy7_task_pos.append(pick_ready_task_pos)
-        self.indy7_task_pos.append(pick_task_pos)
-        self.indy7_task_pos.append(pick_ready_task_pos)
+        # self.indy7_task_pos.append(pick_ready_task_pos)
+        # self.indy7_task_pos.append(pick_task_pos)
+        # self.indy7_task_pos.append(pick_ready_task_pos)
 
-        self.indy7_task_pos.append(release_ready_task_pos)
-        self.indy7_task_pos.append(release_task_pos)
-        self.indy7_task_pos.append(release_ready_task_pos)
+        # self.indy7_task_pos.append(release_ready_task_pos)
+        # self.indy7_task_pos.append(release_task_pos)
+        # self.indy7_task_pos.append(release_ready_task_pos)
+
+        self.indy7_task_pos = [
+            [0.5, -0.3, 0.33, -0.001384742620016496, 179.94928598028307, 0.06657559817585178],
+            [0.5, -0.3, 0.23, -0.001384742620016496, 179.94928598028307, 0.06657559817585178], # pick
+            [0.5, -0.3, 0.33, -0.001384742620016496, 179.94928598028307, 0.06657559817585178],
+            [0.5, -0.05, 0.33, -0.001384742620016496, 179.94928598028307, 0.06657559817585178],
+            [0.5, 0.2, 0.33, -0.001384742620016496, 179.94928598028307, 0.06657559817585178],
+            [0.5, 0.2, 0.23, -0.001384742620016496, 179.94928598028307, 0.06657559817585178], # place
+            [0.5, 0.2, 0.33, -0.001384742620016496, 179.94928598028307, 0.06657559817585178]]
 
     ### make path
     # para switch : string type; path를 joint state, task state, mix state중 어떤것을 사용할지 선택
@@ -113,23 +122,19 @@ class SingleIndy:
         
         if switch == 'joint':
             prog.add_move_home()
-            prog.add_joint_move_to(self.indy7_joint_pos[0], vel=self.vel, blend=self.j_blend)
-            prog.add_joint_move_to(self.indy7_joint_pos[1], vel=self.vel, blend=self.j_blend)
-            prog.add_joint_move_to(self.indy7_joint_pos[2], vel=self.vel, blend=self.j_blend)
-            prog.add_joint_move_to(self.indy7_joint_pos[3], vel=self.vel, blend=self.j_blend)
-            prog.add_joint_move_to(self.indy7_joint_pos[4], vel=self.vel, blend=self.j_blend)
-            prog.add_joint_move_to(self.indy7_joint_pos[5], vel=self.vel, blend=self.j_blend)
+            for way_point in self.indy7_joint_pos:
+                prog.add_joint_move_to(way_point, vel=self.vel, blend=self.j_blend)
             prog.add_move_home()
             prog.add_stop()
 
         elif switch == 'task':
+            prog.add_move_home()
+            self.set_control_config('task', prog)
 
-            prog.add_task_move_to(self.indy7_task_pos[1], vel=self.vel, blend=self.t_blend)
-            prog.add_task_move_to(self.indy7_task_pos[2], vel=self.vel, blend=self.t_blend)
-            prog.add_task_move_to(self.indy7_task_pos[0], vel=self.vel, blend=self.t_blend)
-            prog.add_task_move_to(self.indy7_task_pos[3], vel=self.vel, blend=self.t_blend)
-            prog.add_task_move_to(self.indy7_task_pos[4], vel=self.vel, blend=self.t_blend)
-            prog.add_task_move_to(self.indy7_task_pos[5], vel=self.vel, blend=self.t_blend)
+            for way_point in self.indy7_task_pos:
+                print(way_point)
+                prog.add_task_move_to(way_point, vel=self.vel, blend=self.t_blend)
+
             prog.add_stop()
 
         else:
